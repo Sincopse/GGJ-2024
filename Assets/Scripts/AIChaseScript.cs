d6777dba5a77c42e6203984131c31baf2c8061d2
José Bernardo Ferreira <97bernardoferreira2004@gmail.com>
2024-01-27 18:10:16 +0000

    yeah ai is kinda ok

--- Assets/Scripts/AIChaseScript.cs
+++ Assets/Scripts/AIChaseScript.cs
@@ -1,30 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AIChaseScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
-    public float speed;
+    public float speed, speed2;
     private float distance;
-
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+
         distance = Vector2.Distance(transform.position, player.transform.position);
-        
+
+        Vector2 disY = new Vector2(this.transform.position.x, player.transform.position.y);
+        transform.position = Vector2.MoveTowards(this.transform.position, disY, speed2 * Time.deltaTime);
+
         if (distance > 3)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
 
+
         }
-        
-        
+        if (distance < 3 && (transform.position.y - player.transform.position.y) < 0.1)
+        {
+            //beat his ass
+            
+
+        }
     }
 }
